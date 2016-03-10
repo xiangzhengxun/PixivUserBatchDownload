85b52425cdd913c5869b62b28b9dc2e319a380eb
枫谷剑仙 <mapaler@163.com>
2016-03-10 17:59:10 +0800

    修改说明

--- PixivUesrBatchDownload.user.js
+++ PixivUesrBatchDownload.user.js
@@ -2,7 +2,8 @@
 // @name        PixivUesrBatchDownload
 // @name:zh-CN    P站画师个人作品批量下载工具
 // @namespace   http://www.mapaler.com/
-// @description P站画师个人作品批量下载工具
+// @description Batch download pixiv uesr's images in one key.
+// @description:zh-CN 一键批量下载P站画师的全部作品
 // @include     http://www.pixiv.net/member_illust.php?id=*
 // @include     http://www.pixiv.net/member.php?id=*
 // @include     http://www.pixiv.net/bookmark.php?id=*
