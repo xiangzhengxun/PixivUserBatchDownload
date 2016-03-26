1c92684737daa201a42ba202386b1284ec0c4375
枫谷剑仙 <mapaler@163.com>
2016-03-27 01:13:38 +0800

    修正拼写错误

--- PixivUserBatchDownload.user.js
+++ PixivUserBatchDownload.user.js
@@ -1,16 +1,14 @@
 // ==UserScript==
-// @name        PixivUesrBatchDownload
+// @name        PixivUserBatchDownload
 // @name:zh-CN    P站画师个人作品批量下载工具
 // @namespace   http://www.mapaler.com/
-// @description Batch download pixiv uesr's images in one key.
+// @description Batch download pixiv user's images in one key.
 // @description:zh-CN 一键批量下载P站画师的全部作品
 // @include     http://www.pixiv.net/*
 // @version     1.1.9.2
 // @grant       none
 // @copyright  2016+, Mapaler <mapaler@163.com>
 // @icon        http://source.pixiv.net/www/images/pixiv_logo.gif
-// @updateURL https://github.com/Mapaler/PixivUserBatchDownload/raw/master/PixivUesrBatchDownload.meta.js
-// @downloadURL https://github.com/Mapaler/PixivUserBatchDownload/raw/master/PixivUesrBatchDownload.user.js
 // ==/UserScript==
 
 (function() {
